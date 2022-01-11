1fcf98275e09d54f3c796e2abc448894e9e5515d
Denis Semes <dessnick@gmail.com>
2022-01-11 23:52:04 +0700

    fix: refactoring UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,33 +1,27 @@
 export default class UserInfo {
-  // constructor(userData, userInfoClassList) {
-    constructor(userInfoClassList) {
-    // const { name, about, avatar, _id } = userData;
-    // this._name = name;
-    // this._about = about;
-    // this._avatar = avatar;
-    // this._id = _id;
-
+  constructor(userInfoClassList) {
     this._loginElement = document.querySelector(userInfoClassList.profileName);
     this._aboutElement = document.querySelector(userInfoClassList.profileAbout);
     this._avatarElement = document.querySelector(userInfoClassList.profileAvatar);
   }
 
-  getUserInfo(userData) {
+  getUserInfo() {
     return {
-      name: userData.name,
-      about: userData.about,
-      avatar: userData.avatar,
-      _id: userData._id,
-      // name: this._name,
-      // about: this._about,
-      // avatar: this._avatar,
-      // _id: this._id,
+      name: this._name,
+      about: this._about,
+      avatar: this._avatar,
+      _id: this._id,
     };
   }
 
-  setUserInfo({ name, about, avatar }) {
+  setUserInfo({ name, about, avatar, _id }) {
     this._loginElement.textContent = name;
     this._aboutElement.textContent = about;
     this._avatarElement.src = avatar;
+
+    this._id = _id;
+    this._name = name;
+    this._about = about;
+    this._avatar = avatar;
   }
 }
